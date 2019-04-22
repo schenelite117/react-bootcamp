4410f36da879bd8563a27c032a875680da91d1e0
Stephen Chen <schen@paciolan.com>
2019-04-22 11:06:10 -0700

    Finished projects section, next social profiles challenge

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Projects from "./Projects";
 
 class App extends Component {
+  state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
+
   render() {
+    const bio = this.state.displayBio ? (
+      <div>
+        <button onClick={this.toggleDisplayBio}>Read Less</button>
+        <p>I live in Irvine, CA, and code every day.</p>
+        <p>I think React.js is awesome.</p>
+        <p>Besides coding, I also love to cook and eat awesome food.</p>
+      </div>
+    ) : (
+      <div>
+        <button onClick={this.toggleDisplayBio}>Read More</button>
+      </div>
+    );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Stephen. I'm a software engineer.</p>
+        <p>I'm always looking forward to working on meaningful projects.</p>
+        {bio}
+        <hr />
+        <Projects />
       </div>
     );
   }
